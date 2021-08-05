cac6f9e002e1e2fe8b442147aa3e345f6c6538ed
Jay <milkweedz@hotmail.com>
2021-08-06 02:48:56 +0800

    import svg button icons as react component for customizability

--- inkstone-app/src/components/button/GenericButton.tsx
+++ inkstone-app/src/components/button/GenericButton.tsx
@@ -2,13 +2,13 @@ import {ButtonProps} from '../types/ButtonProps'
 import './GenericButton.scss'
 
 // images
-import rightArrow from '../../images/rightArrow.svg'
+import { ReactComponent as RightArrowSVG } from '../../images/rightArrow.svg'
 
 function GenericButton(props: ButtonProps) {
     return (
         <button className="genericButton" name={props.buttonName} type="button">
             {props.buttonText}
-            {props.buttonIcon?<img className="buttonIcon" src={rightArrow} alt=">"/>:<div/>}
+            {props.buttonIcon?<RightArrowSVG className="buttonIcon"/>:<div/>}
         </button>
     )
 }
--- inkstone-app/src/components/button/InvertedButton.tsx
+++ inkstone-app/src/components/button/InvertedButton.tsx
@@ -2,13 +2,13 @@ import {ButtonProps} from '../types/ButtonProps'
 import './InvertedButton.scss'
 
 // images
-import rightArrow from '../../images/rightArrow.svg'
+import { ReactComponent as RightArrowSVG } from '../../images/rightArrow.svg'
 
 function InvertedButton(props: ButtonProps) {
     return (
         <button className="invertedButton" name={props.buttonName} type="button">
             {props.buttonText}
-            {props.buttonIcon?<img className="buttonIcon" src={rightArrow} alt=">"/>:<div/>}
+            {props.buttonIcon?<RightArrowSVG className="buttonIcon"/>:<div/>}
         </button>
     )
 }
